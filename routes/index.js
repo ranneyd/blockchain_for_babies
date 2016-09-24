93b149b0b43ec9a8b64adbfe05570bdbf0d40a6e
Micah Bunting <micah@Jeffs-MacBook-Pro.local>
2016-09-24 13:22:05 -0500

    Add signup page

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
+/* GET signup page. */
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
 module.exports = router;
