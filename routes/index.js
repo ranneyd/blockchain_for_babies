5e31a975462e7415166856e3d32b87e5bb688f36
Dustin Kane <Dustin Kane>
2016-09-24 10:50:38 -0500

    Added favicon, homepage etc

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'babies.io' });
+  res.render('index', { title: 'Blockchain for Babies' });
 });
 
 module.exports = router;
