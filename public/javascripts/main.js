d22016823ce018efd11957339d2a7f1e9c1b56de
Micah Bunting <micah@Jeffs-MacBook-Pro.local>
2016-09-24 15:04:04 -0500

    use date picker for birthdate

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,7 @@
 $('.button-collapse').sideNav();
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+
