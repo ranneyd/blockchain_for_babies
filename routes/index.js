e4f4352c7abf77e42f83b6acb3f86327b4ee13fe
Dustin Kane <Dustin Kane>
2016-09-24 12:06:38 -0500

    Added login page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Blockchain for Babies' });
 });
 
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 module.exports = router;
