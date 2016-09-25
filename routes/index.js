6157dc497697c6227f4f8f94b77eace8aaa0af2b
Micah Bunting <micah@Jeffs-MacBook-Pro.local>
2016-09-24 20:59:20 -0500

    add backend for createparent form

--- routes/index.js
+++ routes/index.js
@@ -53,8 +53,20 @@ router.get('/createparent', function(req, res, next) {
   res.render('createparent');
 });
 
-router.post('/createparent', function(req, res, next) {
-  res.render('createparent');
+router.post('/createparent', function(req, res) {
+    console.log(req.body);
+    if(req.body.password !== req.body.password2) {
+        return res.render('signup', { error : "Passwords don't match" });
+    }
+    User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
+        if (err) {
+            return res.render('createparent', { error : err.message });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/home');
+        });
+    });
 });
 
 router.get('/createbaby', function(req, res, next) {
