1d771269cc136661fd7fe9220bf325dc23a4ebcb
Dan Hedges <daniel.a.hedges@gmail.com>
2016-09-24 19:57:56 -0500

    fixing init scripts to add a salted hashed password for admin/admin

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -1,10 +1,4 @@
 #! /bin/sh
 # initdb.sh
-#
-# Reset the BabyID mongo development database back to init.
-# Deletes the entire contents of the db directory, if one exists.
-# Then, initializes a new database with default admin credentials.
 
-rm -rf db/*
-mongod --dbpath db &
-mongo babyid --eval "db.users.insert({username: 'admin', password: 'admin', admin: true});"
+mongo babyid scripts/firstUser.js
